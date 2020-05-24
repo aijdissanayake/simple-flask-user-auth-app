d040eac502eaaa5cb268b6b95317f163a0103d1a
Achala Dissanayake <aijdissanayake@gmail.com>
2020-05-24 09:49:40 +0100

    refinements

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.secret_key = 'your secret key'
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'pythonlogin'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pythonlogin'
 
